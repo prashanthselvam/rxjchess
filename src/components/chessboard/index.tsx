de27bebf053b537bd0e7f9a0985b4cae91722a1b
Prashanth Selvam <prashanth.selvam@gmail.com>
2021-02-21 23:19:52 -0500

    Make thing a little bit responsive

--- src/components/chessboard/index.tsx
+++ src/components/chessboard/index.tsx
@@ -15,8 +15,16 @@ const Chessboard = () => {
 
   const styles = css`
     transition: all 0.5s;
-    width: ${isGameInitializingOrInProgress ? "38%" : "80%"};
-    margin-top: ${isGameInitializingOrInProgress ? "1.5rem" : "-66%"};
+    width: 90%;
+    margin-top: ${isGameInitializingOrInProgress ? "1.5rem" : "-76%"};
+    @media (min-width: 768px) {
+      width: ${isGameInitializingOrInProgress ? "60%" : "80%"};
+      margin-top: ${isGameInitializingOrInProgress ? "1.5rem" : "-66%"};
+    }
+    @media (min-width: 1024px) {
+      width: ${isGameInitializingOrInProgress ? "38%" : "80%"};
+      margin-top: ${isGameInitializingOrInProgress ? "1.5rem" : "-66%"};
+    }
   `;
 
   return (
