5c9fa4679a8bbec44ba2a8234158437490940065
Prashanth Selvam <prashanth.selvam@gmail.com>
2021-03-09 00:37:26 -0500

    Incoporated chess engineeee

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,3 +7,19 @@ exports.onCreateWebpackConfig = ({ stage, actions, plugins }) => {
     ],
   });
 };
+
+exports.onCreateWebpackConfig = ({
+  actions: { replaceWebpackConfig },
+  getConfig,
+}) => {
+  const config = getConfig();
+
+  config.module.rules.push({
+    test: /\.worker\.js$/,
+    use: { loader: "workerize-loader" },
+  });
+
+  config.output.globalObject = "this";
+
+  replaceWebpackConfig(config);
+};
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { store } from "src/store";
+import { actions, Move, store } from "src/store";
 import { Provider } from "react-redux";
 import * as React from "react";
 import GameOptions from "./GameOptions";
@@ -8,9 +8,36 @@ import Cockpit from "./Cockpit";
 import Chessboard from "./chessboard";
 import { useSelector } from "react-redux";
 
+const jsChess = require("js-chess-engine");
+
 const Game = () => {
   const gameStatus = useSelector((state) => state.gameStatus);
+  const moveHistory = useSelector((state) => state.movesState.moveHistory);
   const isGameActive = ["IN PROGRESS", "READY"].includes(gameStatus);
+  const game = React.useMemo(() => new jsChess.Game(), []);
+
+  React.useEffect(() => {
+    if (gameStatus !== "IN PROGRESS") {
+      return;
+    }
+
+    const lastMove: Move = moveHistory.slice(-1)[0];
+
+    if (lastMove.player === "W") {
+      setTimeout(() => {
+        game.move(lastMove.sourceTileId, lastMove.targetTileId);
+        game.aiMove(2);
+        const aiMove = game.getHistory(true)[0];
+        store.dispatch(actions.selectTile({ tileId: aiMove.from }));
+        store.dispatch(
+          actions.moveToTile({
+            sourceTileId: aiMove.from,
+            targetTileId: aiMove.to,
+          })
+        );
+      }, 1000);
+    }
+  }, [moveHistory]);
 
   return (
     <>
